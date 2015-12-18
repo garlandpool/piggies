9ed93747c6df9f2ae12436abc7737f32e45a032a
garlandpool <frederic.pool@threerivers.k12.or.us>
2015-12-17 20:45:41 -0800

    Fix2

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,6 @@
-# class AdminsController < ApplicationController
-# end
-class Admins::RegistrationsController < Devise::RegistrationsController
-	devise_for :admins, :controllers => { :sessions => "admins/sessions" }
-	devise_for :admins, :controllers => { :registrations => "admins/registrations" }
+class AdminsController < ApplicationController
 end
+# class Admins::RegistrationsController < Devise::RegistrationsController
+# 	devise_for :admins, :controllers => { :sessions => "admins/sessions" }
+# 	devise_for :admins, :controllers => { :registrations => "admins/registrations" }
+# end
